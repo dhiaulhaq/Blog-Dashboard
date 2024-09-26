15d96298ebdc5bd92c3edc57d4a7c058ba39bc64
dhiaulhaq <dmnaufal7@gmail.com>
2024-09-26 11:35:30 +0700

    feat: add detail post

--- pub/src/components/Card.jsx
+++ pub/src/components/Card.jsx
@@ -1,30 +1,36 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const navigate = useNavigate();
+
+function handleClick(id) {
+  navigate(`/${post.id}`);
+}
+
 export default function Card({ post }) {
   return (
     <>
       <div className="rounded overflow-hidden shadow-lg flex flex-col">
-        <a href="#" />
+        <Link to={`/${post.id}`} />
         <div className="relative">
-          <a href="#">
+          <Link to={`/${post.id}`}>
             <img
               className="w-full"
               src={post.imgUrl}
               alt="Sunset in the mountains"
             />
             <div className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25"></div>
-          </a>
-          <a href="#!">
-            <div className="text-xs absolute top-0 right-0 bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
-              {post.Category.name}
-            </div>
-          </a>
+          </Link>
+          <div className="text-xs absolute top-0 right-0 bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+            {post.Category.name}
+          </div>
         </div>
         <div className="px-6 py-4 mb-auto">
-          <a
-            href="#"
+          <Link
+            to={`/${post.id}`}
             className="font-medium text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2"
           >
             {post.title}
-          </a>
+          </Link>
           <p className="text-gray-500 text-sm">{post.content}</p>
         </div>
       </div>
